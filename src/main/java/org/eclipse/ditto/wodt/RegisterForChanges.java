55cc83be6e45d1aa9c079c90a6813363a613e4a3
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2024-08-09 12:29:51 +0200

    Edit project files

--- src/main/java/org/eclipse/ditto/wodt/RegisterForChanges.java
+++ src/main/java/org/eclipse/ditto/wodt/RegisterForChanges.java
@@ -1,19 +1,9 @@
 package org.eclipse.ditto.wodt;
 
-import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
-import org.eclipse.ditto.base.model.auth.AuthorizationSubject;
 import org.eclipse.ditto.client.DittoClient;
-import org.eclipse.ditto.client.changes.ChangeAction;
-import org.eclipse.ditto.client.management.ThingHandle;
-import org.eclipse.ditto.json.JsonPointer;
-import org.eclipse.ditto.json.JsonValue;
-import org.eclipse.ditto.things.model.Thing;
-import org.eclipse.ditto.things.model.ThingId;
 import org.eclipse.ditto.wodt.common.ExamplesBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,7 +13,6 @@ public final class RegisterForChanges extends ExamplesBase {
     private static final Logger LOGGER = LoggerFactory.getLogger(RegisterForChanges.class);
 
     private final CountDownLatch countDownLatch;
-    private final ThingId thingId = randomThingId();
 
     private RegisterForChanges() {
         super();
@@ -31,11 +20,7 @@ public final class RegisterForChanges extends ExamplesBase {
 
         try {
             registerForThingChanges(client1);
-            //registerForThingChangesWithDeregistration(client1);
-
             startConsumeChanges(client1);
-
-            //createThing(client2, authorizationSubject);
         } finally {
             destroy();
         }
@@ -45,65 +30,15 @@ public final class RegisterForChanges extends ExamplesBase {
         new RegisterForChanges();
     }
 
-    private static String registrationId() {
-        return "registration:" + UUID.randomUUID();
-    }
-
     /**
      * Register for all {@code ThingChange}s.
      */
     private void registerForThingChanges(final DittoClient client) {
-        final ThingHandle thingHandle = client.twin().forId(thingId);
-
-        client.twin().registerForThingChanges(registrationId(), change -> {
-            LOGGER.info("For all things: ThingChange received: {}", change);
-            countDownLatch.countDown();
-        });
-
-        thingHandle.registerForThingChanges(registrationId(),
-                change -> LOGGER.info("My Thing: ThingChange received: {}", change));
-    }
-
-    /**
-     * Register for {@code ThingChange}s and deregister after the created-event has been retrieved.
-     */
-    private void registerForThingChangesWithDeregistration(final DittoClient client) {
-        final ThingHandle thingHandle = client.twin().forId(thingId);
-
-        final String registrationId = registrationId();
-        LOGGER.info("RegistrationId: {}", registrationId);
-
-        thingHandle.registerForThingChanges(registrationId, change -> {
-            LOGGER.info("{}: ThingChange received: {}", thingId, change);
-
-            /* Deregister when the created-event has been retrieved */
-            if (change.getAction() == ChangeAction.CREATED) {
-                LOGGER.info("{}: Deregister handler with id: {}", thingId, registrationId);
-                thingHandle.deregister(registrationId);
-                countDownLatch.countDown();
-            }
-        });
-    }
-
-    private void createThing(final DittoClient client, final AuthorizationSubject... subjects) {
-        LOGGER.info("Create thing {} and set required permissions.", thingId);
-
-        final Thing thing = Thing.newBuilder()
-                .setId(thingId)
-                .build();
-
-        try {
-            client.twin().create(thing)
-                    .thenCompose(createdThing -> {
-                        final Thing updatedThing = createdThing.toBuilder()
-                                .setAttribute(JsonPointer.of("foo"), JsonValue.of("bar"))
-                                .build();
-                        return client.twin().update(updatedThing);
-                    }).toCompletableFuture()
-                    .get(10, TimeUnit.SECONDS);
-        } catch (final InterruptedException | ExecutionException | TimeoutException e) {
-            throw new IllegalStateException(e);
-        }
+        client.twin().search()
+            .stream(queryBuilder -> queryBuilder.namespace("org.eclipse.ditto")
+                .options(builder -> builder.sort(s -> s.desc("thingId")))
+            )
+            .forEach(foundThing -> System.out.println("Found thing: " + foundThing));
     }
 
     private void destroy() {
---
+++ src/main/java/org/eclipse/ditto/wodt/Server.java
@@ -0,0 +1,12 @@
+package org.eclipse.ditto.wodt;
+
+/*
+ * Module entrypoint
+ */
+public class Server {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+    
+}
